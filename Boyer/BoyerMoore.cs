aac50019f4ded329d9f343a46a07fd698eb461bd
Adrian Sims <adriansims62781@gmail.com>
2020-03-27 14:05:50 +0100

    Add project files.

---
+++ Boyer/BoyerMoore.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boyer
+{
+    static class BoyerMoore
+    {
+        public static int[] SearchString(byte[] bytes, string pattern)
+        {
+            List<int> result = new List<int>();
+            var bytesToSearch = Encoding.Unicode.GetBytes(pattern);
+
+            int m = bytesToSearch.Length;
+            int n = bytes.Length;
+
+            int[] badChar = new int[256];
+
+            BadCharHeuristic(bytesToSearch, m, ref badChar);
+
+            int s = 0;
+            while (s <= (n - m))
+            {
+                int j = m - 1;
+
+                while (j >= 0 && bytesToSearch[j] == bytes[s + j])
+                    --j;
+
+                if (j < 0)
+                {
+                    result.Add(s);
+                    s += (s + m < n) ? m - badChar[bytes[s + m]] : 1;
+                }
+                else
+                {
+                    s += Math.Max(1, j - badChar[bytes[s + j]]);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private static void BadCharHeuristic(byte[] str, int size, ref int[] badChar)
+        {
+            int i;
+
+            for (i = 0; i < 256; i++)
+                badChar[i] = -1;
+
+            for (i = 0; i < size; i++)
+                badChar[(int)str[i]] = i;
+        }
+    }
+}
---
+++ Boyer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boyer
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var dir = Path.GetDirectoryName(fullname);
+                var testDir = Path.Combine(new DirectoryInfo(dir).Parent.Parent.FullName, "TestData");
+                var filename = Path.Combine(testDir, "ada.exe");
+                var bytes = File.ReadAllBytes(filename);
+                var rlist = BoyerMoore.SearchString(bytes, "TestApplicationToken");
+                if (rlist.Length == 0)
+                {
+                    throw new Exception("No matches");
+                }
+                if (rlist.Length != 1)
+                {
+                    Console.Error.WriteLine("Warning: found more than one match");
+                }
+
+                var encodedReplacement = Encoding.Unicode.GetBytes("DebugToken").ToList();
+                encodedReplacement.AddRange(new byte[] { 0, 0 });
+
+                encodedReplacement.CopyTo(bytes, rlist.First());
+                var newFilename = Path.ChangeExtension(filename, "exe2");
+                File.WriteAllBytes(newFilename, bytes);
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+        }
+    }
+}
